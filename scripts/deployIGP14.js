32f51d85848221ed7ec4f199c73af975d6e7d0b8
Thrilok kumar <thrilok2000@gmail.com>
2024-03-21 19:26:16 -0400

    fix

--- scripts/deployIGP14.js
+++ scripts/deployIGP14.js
@@ -2,14 +2,14 @@ const hre = require("hardhat");
 const { ethers } = hre;
 
 async function main() {
-  // const PayloadIGP14 = await ethers.getContractFactory("PayloadIGP14")
-  // const payloadIGP14 = await PayloadIGP14.deploy()
-  // await payloadIGP14.deployed()
+  const PayloadIGP14 = await ethers.getContractFactory("PayloadIGP14")
+  const payloadIGP14 = await PayloadIGP14.deploy()
+  await payloadIGP14.deployed()
 
-  // console.log("PayloadIGP14: ", payloadIGP14.address)
+  console.log("PayloadIGP14: ", payloadIGP14.address)
 
   await hre.run("verify:verify", {
-    address: "0x9C5F9e5987EBc5cb589215d6cE9Af8FE72560AE8",
+    address: payloadIGP14.address,
     constructorArguments: []
   })
 }
