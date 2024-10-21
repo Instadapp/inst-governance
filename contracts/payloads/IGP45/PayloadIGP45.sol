734c85a0378035971bf192b0dbd523ffceb7ff2e
0xprinc <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@gmail.com>
2024-10-22 02:17:11 +0530

    .

--- contracts/payloads/IGP45/PayloadIGP45.sol
+++ contracts/payloads/IGP45/PayloadIGP45.sol
@@ -229,6 +229,33 @@ interface IFluidLiquidityAdmin {
     ) external view returns (uint256 result_);
 }
 
+interface IProxy {
+    function setAdmin(address newAdmin_) external;
+
+    function setDummyImplementation(address newDummyImplementation_) external;
+
+    function addImplementation(
+        address implementation_,
+        bytes4[] calldata sigs_
+    ) external;
+
+    function removeImplementation(address implementation_) external;
+
+    function getAdmin() external view returns (address);
+
+    function getDummyImplementation() external view returns (address);
+
+    function getImplementationSigs(
+        address impl_
+    ) external view returns (bytes4[] memory);
+
+    function getSigsImplementation(bytes4 sig_) external view returns (address);
+
+    function readFromStorage(
+        bytes32 slot_
+    ) external view returns (uint256 result_);
+}
+
 interface FluidVaultFactory {
     /// @notice                         Sets an address as allowed vault deployment logic (`deploymentLogic_`) contract or not.
     ///                                 This function can only be called by the owner.
