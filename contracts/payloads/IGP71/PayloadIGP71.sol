8d505374ad446dd078af58557ba6e350ea6aa549
Samyak Jain <34437877+KaymasJain@users.noreply.github.com>
2024-12-30 00:41:18 +0800

    update: hardcoding the code

--- contracts/payloads/IGP71/PayloadIGP71.sol
+++ contracts/payloads/IGP71/PayloadIGP71.sol
@@ -81,13 +81,19 @@ contract PayloadIGP71 is PayloadIGPConstants, PayloadIGPHelpers {
     /// @notice Action 1: Increase Allowance and LTV of stETH redemption protocol
     function action1() internal {
         {
-            // Increase Allowance to 10k ETH
-            uint256 amount_ = getRawAmount(
-                ETH_ADDRESS,
-                10_000 * 1e18,
-                0,
-                false
+            uint256 exchangePriceAndConfig_ = LIQUIDITY.readFromStorage(
+                LiquiditySlotsLink.calculateMappingStorageSlot(
+                    LiquiditySlotsLink.LIQUIDITY_EXCHANGE_PRICES_MAPPING_SLOT,
+                    ETH_ADDRESS
+                )
             );
+
+            (
+                uint256 supplyExchangePrice,
+                uint256 borrowExchangePrice
+            ) = LiquidityCalcs.calcExchangePrices(exchangePriceAndConfig_);
+
+            amount_ = (10_000 * 1e18 * 1e12) / borrowExchangePrice;
 
             // Borrow Limits
             FluidLiquidityAdminStructs.UserBorrowConfig[]
@@ -99,111 +105,13 @@ contract PayloadIGP71 is PayloadIGPConstants, PayloadIGPHelpers {
                 user: address(0x1F6B2bFDd5D1e6AdE7B17027ff5300419a56Ad6b),
                 token: ETH_ADDRESS,
                 mode: 1,
-                expandPercent: 0,
+                expandPercent: 1,
                 expandDuration: 1,
                 baseDebtCeiling: amount_,
-                maxDebtCeiling: amount_
+                maxDebtCeiling: (amount_ * 1001) / 1000
             });
 
             LIQUIDITY.updateUserBorrowConfigs(configs_);
-        }
-    }
-
-    /**
-     * |
-     * |     Proposal Payload Helpers      |
-     * |__________________________________
-     */
-
-    // Token Prices Constants
-    uint256 public constant ETH_USD_PRICE = 3_400 * 1e2;
-    uint256 public constant wstETH_USD_PRICE = 4_750 * 1e2;
-    uint256 public constant weETH_USD_PRICE = 4_250 * 1e2;
-    uint256 public constant rsETH_USD_PRICE = 4_150 * 1e2;
-    uint256 public constant weETHs_USD_PRICE = 4_050 * 1e2;
-    uint256 public constant mETH_USD_PRICE = 4_050 * 1e2;
-
-    uint256 public constant BTC_USD_PRICE = 95_000 * 1e2;
-
-    uint256 public constant STABLE_USD_PRICE = 1 * 1e2;
-    uint256 public constant sUSDe_USD_PRICE = 1 * 1e2;
-    uint256 public constant sUSDs_USD_PRICE = 1 * 1e2;
-
-    uint256 public constant INST_USD_PRICE = 7 * 1e2;
-
-    function getRawAmount(
-        address token,
-        uint256 amount,
-        uint256 amountInUSD,
-        bool isSupply
-    ) public view override returns (uint256) {
-        if (amount > 0 && amountInUSD > 0) {
-            revert("both usd and amount are not zero");
-        }
-        uint256 exchangePriceAndConfig_ = LIQUIDITY.readFromStorage(
-            LiquiditySlotsLink.calculateMappingStorageSlot(
-                LiquiditySlotsLink.LIQUIDITY_EXCHANGE_PRICES_MAPPING_SLOT,
-                token
-            )
-        );
-
-        (
-            uint256 supplyExchangePrice,
-            uint256 borrowExchangePrice
-        ) = LiquidityCalcs.calcExchangePrices(exchangePriceAndConfig_);
-
-        uint256 usdPrice = 0;
-        uint256 decimals = 18;
-        if (token == ETH_ADDRESS) {
-            usdPrice = ETH_USD_PRICE;
-            decimals = 18;
-        } else if (token == wstETH_ADDRESS) {
-            usdPrice = wstETH_USD_PRICE;
-            decimals = 18;
-        } else if (token == weETH_ADDRESS) {
-            usdPrice = weETH_USD_PRICE;
-            decimals = 18;
-        } else if (token == rsETH_ADDRESS) {
-            usdPrice = rsETH_USD_PRICE;
-            decimals = 18;
-        } else if (token == weETHs_ADDRESS) {
-            usdPrice = weETHs_USD_PRICE;
-            decimals = 18;
-        } else if (token == mETH_ADDRESS) {
-            usdPrice = mETH_USD_PRICE;
-            decimals = 18;
-        } else if (token == cbBTC_ADDRESS || token == WBTC_ADDRESS) {
-            usdPrice = BTC_USD_PRICE;
-            decimals = 8;
-        } else if (token == USDC_ADDRESS || token == USDT_ADDRESS) {
-            usdPrice = STABLE_USD_PRICE;
-            decimals = 6;
-        } else if (token == sUSDe_ADDRESS) {
-            usdPrice = sUSDe_USD_PRICE;
-            decimals = 18;
-        } else if (token == sUSDs_ADDRESS) {
-            usdPrice = sUSDs_USD_PRICE;
-            decimals = 18;
-        } else if (token == GHO_ADDRESS || token == USDe_ADDRESS) {
-            usdPrice = STABLE_USD_PRICE;
-            decimals = 18;
-        } else if (token == INST_ADDRESS) {
-            usdPrice = INST_USD_PRICE;
-            decimals = 18;
-        } else {
-            revert("not-found");
-        }
-
-        uint256 exchangePrice = isSupply
-            ? supplyExchangePrice
-            : borrowExchangePrice;
-
-        if (amount > 0) {
-            return (amount * 1e12) / exchangePrice;
-        } else {
-            return
-                (amountInUSD * 1e12 * (10 ** decimals)) /
-                ((usdPrice * exchangePrice) / 1e2);
         }
     }
 }
