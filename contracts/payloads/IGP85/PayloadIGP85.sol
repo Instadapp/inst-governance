e5ab596b58d4736135f7bd6ba32f7018e99e85d4
gite <55936621+xma8lu@users.noreply.github.com>
2025-02-15 01:13:29 +0530

    add IGP85

---
+++ contracts/payloads/IGP85/PayloadIGP85.sol
@@ -0,0 +1,563 @@
+pragma solidity ^0.8.21;
+pragma experimental ABIEncoderV2;
+
+import {BigMathMinified} from "../libraries/bigMathMinified.sol";
+import {LiquidityCalcs} from "../libraries/liquidityCalcs.sol";
+import {LiquiditySlotsLink} from "../libraries/liquiditySlotsLink.sol";
+
+import {IGovernorBravo} from "../common/interfaces/IGovernorBravo.sol";
+import {ITimelock} from "../common/interfaces/ITimelock.sol";
+
+import {IFluidLiquidityAdmin, AdminModuleStructs as FluidLiquidityAdminStructs} from "../common/interfaces/IFluidLiquidity.sol";
+import {IFluidReserveContract} from "../common/interfaces/IFluidReserveContract.sol";
+
+import {IFluidVaultFactory} from "../common/interfaces/IFluidVaultFactory.sol";
+import {IFluidDexFactory} from "../common/interfaces/IFluidDexFactory.sol";
+
+import {IFluidDex, IFluidAdminDex, IFluidDexResolver} from "../common/interfaces/IFluidDex.sol";
+
+import {IFluidVault, IFluidVaultT1} from "../common/interfaces/IFluidVault.sol";
+
+import {IFTokenAdmin, ILendingRewards} from "../common/interfaces/IFToken.sol";
+
+import {IDSAV2} from "../common/interfaces/IDSA.sol";
+import {IERC20} from "../common/interfaces/IERC20.sol";
+import {IProxy} from "../common/interfaces/IProxy.sol";
+import {PayloadIGPConstants} from "../common/constants.sol";
+import {PayloadIGPHelpers} from "../common/helpers.sol";
+import {PayloadIGPMain} from "../common/main.sol";
+
+contract PayloadIGP85 is PayloadIGPMain {
+    uint256 public constant PROPOSAL_ID = 85;
+
+    address public USDC_USDT_FEE_HANDLER;
+
+    function execute() public virtual override {
+        super.execute();
+
+        // Action 1: Set launch limits for cbBTC-USDT DEX T4 Vault
+        action1();
+
+        // Action 2: Set dust limits for cbBTC-ETH DEX T4 vault
+        action2();
+
+        // Action 3: Reset launch limits for ezETH-ETH DEX and ezETH<>wstETH T1 & ezETH-ETH<>wstETH T2 vaults
+        action3();
+
+        // Action 4: Update rewards for fUSDC, fUSDT
+        action4();
+
+        // Action 5: Constrict BOLD DEX 
+        action5();
+    }
+
+    function verifyProposal() public view override {}
+
+    function _PROPOSAL_ID() internal view override returns (uint256) {
+        return PROPOSAL_ID;
+    }
+
+
+    /**
+     * |
+     * |    Team Multisig Function        |
+     * |__________________________________
+     */
+
+    function setState(address USDC_USDT_FEE_HANDLER_) external {
+        if (msg.sender != TEAM_MULTISIG) {
+            revert("not-team-multisig");
+        }
+        USDC_USDT_FEE_HANDLER = USDC_USDT_FEE_HANDLER_;
+    }
+    
+
+    /**
+     * |
+     * |     Proposal Payload Actions      |
+     * |__________________________________
+     */
+
+    // @notice Action 1: Set launch limits for cbBTC-USDT DEX T4 Vault
+    function action1() internal isActionSkippable(1) {
+        address cbBTC_USDT_DEX_ADDRESS = getDexAddress(22);
+
+        {
+            // dust limits
+            Dex memory DEX_cbBTC_USDT = Dex({
+                dex: cbBTC_USDT_DEX_ADDRESS,
+                tokenA: cbBTC_ADDRESS,
+                tokenB: USDT_ADDRESS,
+                smartCollateral: true,
+                smartDebt: true,
+                baseWithdrawalLimitInUSD: 15_000_000, // $15M
+                baseBorrowLimitInUSD: 15_000_000, // $15M
+                maxBorrowLimitInUSD: 20_000_000 // $20M
+            });
+            setDexLimits(DEX_cbBTC_USDT); // Smart Collateral & Smart Debt
+
+            DEX_FACTORY.setDexAuth(cbBTC_USDT_DEX_ADDRESS, TEAM_MULTISIG, false);
+        }
+
+        {
+            address cbBTC_USDT__cbBTC_USDT_VAULT_ADRESS = getVaultAddress(105);
+
+            // Set team multisig as vault auth for cbBTC_USDT T4 Vault
+            VAULT_FACTORY.setVaultAuth(
+                cbBTC_USDT__cbBTC_USDT_VAULT_ADRESS,
+                TEAM_MULTISIG,
+                false
+            );
+        }
+    }
+
+    // @notice Action 2: Set dust limits for cbBTC-ETH DEX T4 vault
+    function action2() internal {
+        address cbBTC_ETH_DEX_ADDRESS = getDexAddress(27);
+
+        {
+            // dust limits
+            Dex memory DEX_cbBTC_ETH = Dex({
+                dex: cbBTC_ETH_DEX_ADDRESS,
+                tokenA: cbBTC_ADDRESS,
+                tokenB: ETH_ADDRESS,
+                smartCollateral: true,
+                smartDebt: true,
+                baseWithdrawalLimitInUSD: 10_000, // $10k
+                baseBorrowLimitInUSD: 8_000, // $8k
+                maxBorrowLimitInUSD: 10_000 // $10k
+            });
+            setDexLimits(DEX_cbBTC_ETH); // Smart Collateral & Smart Debt
+
+            DEX_FACTORY.setDexAuth(cbBTC_ETH_DEX_ADDRESS, TEAM_MULTISIG, true);
+        }
+
+        {
+            address cbBTC_ETH__cbBTC_ETH_VAULT_ADRESS = getVaultAddress(106);
+
+            // Set team multisig as vault auth for cbBTC_USDT T4 Vault
+            VAULT_FACTORY.setVaultAuth(
+                cbBTC_ETH__cbBTC_ETH_VAULT_ADRESS,
+                TEAM_MULTISIG,
+                true
+            );
+        }
+    }
+
+    // @notice Action 3: Reset dust limits for ezETH-ETH DEX and ezETH<>wstETH T1 & ezETH-ETH<>wstETH T2 vaults
+    function action3() internal isActionSkippable(3) {
+        {
+            address ezETH_ETH_DEX = getDexAddress(21);
+            // ezETH-ETH DEX
+            {
+                // ezETH-ETH Dex
+                Dex memory DEX_ezETH_ETH = Dex({
+                    dex: ezETH_ETH_DEX,
+                    tokenA: ezETH_ADDRESS,
+                    tokenB: ETH_ADDRESS,
+                    smartCollateral: true,
+                    smartDebt: false,
+                    baseWithdrawalLimitInUSD: 7_500_000, // $7.5M
+                    baseBorrowLimitInUSD: 0, // $0
+                    maxBorrowLimitInUSD: 0 // $0
+                });
+                setDexLimits(DEX_ezETH_ETH); // Smart Collateral
+
+                DEX_FACTORY.setDexAuth(ezETH_ETH_DEX, TEAM_MULTISIG, false);
+            }
+        }
+
+        {
+            address ezETH__wstETH_VAULT = getVaultAddress(103);
+
+            // [TYPE 1] ezETH<>wstETH | normal collateral & normal debt
+            Vault memory VAULT_ezETH_wstETH = Vault({
+                vault: ezETH__wstETH_VAULT,
+                vaultType: TYPE.TYPE_1,
+                supplyToken: ezETH_ADDRESS,
+                borrowToken: wstETH_ADDRESS,
+                baseWithdrawalLimitInUSD: 10_000_000, // $10M
+                baseBorrowLimitInUSD: 10_000_000, // $10M
+                maxBorrowLimitInUSD: 20_000_000 // $20M
+            });
+
+            setVaultLimits(VAULT_ezETH_wstETH); // TYPE_1 => 103
+
+            VAULT_FACTORY.setVaultAuth(
+                ezETH__wstETH_VAULT,
+                TEAM_MULTISIG,
+                false
+            );
+        }
+
+        {
+            address ezETH_ETH__wstETH_VAULT = getVaultAddress(104);
+
+            // [TYPE 2] ezETH-ETH<>wstETH | smart collateral & normal debt
+            Vault memory VAULT_ezETH_ETH_wstETH = Vault({
+                vault: ezETH_ETH__wstETH_VAULT,
+                vaultType: TYPE.TYPE_2,
+                supplyToken: address(0),
+                borrowToken: wstETH_ADDRESS,
+                baseWithdrawalLimitInUSD: 0,
+                baseBorrowLimitInUSD: 7_500_000, // $7.5M
+                maxBorrowLimitInUSD: 15_000_000 // $15M
+            });
+
+            setVaultLimits(VAULT_ezETH_ETH_wstETH); // TYPE_2 => 104
+
+            VAULT_FACTORY.setVaultAuth(
+                ezETH_ETH__wstETH_VAULT,
+                TEAM_MULTISIG,
+                false
+            );
+        }
+    }
+
+    // @notice Action 4: Update rewards for fUSDC, fUSDT
+    function action4() internal isActionSkippable(4) {
+        address[] memory protocols = new address[](2);
+        address[] memory tokens = new address[](2);
+        uint256[] memory amounts = new uint256[](2);
+
+        address REWARDS_ADDRESS = address(
+            //0x
+        );
+
+        {
+            /// fUSDC
+            IFTokenAdmin(F_USDC).updateRewards(REWARDS_ADDRESS);
+
+            uint256 allowance = IERC20(USDC_ADDRESS).allowance(
+                address(FLUID_RESERVE),
+                F_USDC
+            );
+
+            protocols[0] = F_USDC;
+            tokens[0] = USDC_ADDRESS;
+            amounts[0] = allowance ;
+        }
+
+        {
+            /// fUSDT
+            IFTokenAdmin(F_USDT).updateRewards(REWARDS_ADDRESS);
+
+            uint256 allowance = IERC20(USDT_ADDRESS).allowance(
+                address(FLUID_RESERVE),
+                F_USDT
+            );
+
+            protocols[1] = F_USDT;
+            tokens[1] = USDT_ADDRESS;
+            amounts[1] = allowance ;
+        }
+
+        FLUID_RESERVE.approve(protocols, tokens, amounts);
+
+        {
+            ILendingRewards(REWARDS_ADDRESS).start();
+        }
+    }
+
+    // @notice Action 5: Constrict BOLD DEX
+    function action5(){
+
+        {
+            address USDC_BOLD_DEX = getDexAddress(25);
+            // USDC-BOLD DEX
+            {
+                // USDC-BOLD Dex
+                Dex memory DEX_USDC_BOLD = Dex({
+                    dex: USDC_BOLD_DEX,
+                    tokenA: USDC_ADDRESS,
+                    tokenB: BOLD_ADDRESS,
+                    smartCollateral: true,
+                    smartDebt: false,
+                    baseWithdrawalLimitInUSD: 10, // $10
+                    baseBorrowLimitInUSD: 0, // $0
+                    maxBorrowLimitInUSD: 0 // $0
+                });
+                setDexLimits(DEX_USDC_BOLD); // Smart Collateral
+
+                DEX_FACTORY.setDexAuth(USDC_BOLD_DEX, TEAM_MULTISIG, false);
+            }
+        }
+
+        {
+            address USDC_BOLD_fSL20 = 0x9b1f75ea07723F331996831f6d04AD4900d1A3B3;
+
+            // Update USDC-BOLD smart lending vault supply limit with minimum value
+            IFluidAdminDex.UserSupplyConfig[]
+                memory config_ = new IFluidAdminDex.UserSupplyConfig[](1);
+                config_[0] = IFluidAdminDex.UserSupplyConfig({
+                user: USDC_BOLD_fSL20,
+                expandPercent: 1 * 1e2, // 1%
+                expandDuration: 999 days, // max time
+                baseWithdrawalLimit: 1 * 1e18 // 1 shares
+            });
+
+            IFluidDex(USDC_BOLD_fSL20).updateUserSupplyConfigs(config_);
+        }
+
+        {
+            // Pause user supply and borrow
+            address[] memory supplyTokens = new address[](2);
+            supplyTokens[0] = USDC_ADDRESS;
+            supplyTokens[1] = BOLD_ADDRESS;
+
+            address[] memory borrowTokens = new address[](2);
+            borrowTokens[0] = address(0);
+            borrowTokens[1] = address(0);;
+
+            // Pause the user operations
+            pauseUser(USDC_BOLD_DEX, supplyTokens, borrowTokens);
+        }
+    }
+
+    /**
+     * |
+     * |     Proposal Payload Helpers      |
+     * |__________________________________
+     */
+    struct Dex {
+        address dex;
+        address tokenA;
+        address tokenB;
+        bool smartCollateral;
+        bool smartDebt;
+        uint256 baseWithdrawalLimitInUSD;
+        uint256 baseBorrowLimitInUSD;
+        uint256 maxBorrowLimitInUSD;
+    }
+
+    enum TYPE {
+        TYPE_1,
+        TYPE_2,
+        TYPE_3,
+        TYPE_4
+    }
+
+    struct Vault {
+        address vault;
+        TYPE vaultType;
+        address supplyToken;
+        address borrowToken;
+        uint256 baseWithdrawalLimitInUSD;
+        uint256 baseBorrowLimitInUSD;
+        uint256 maxBorrowLimitInUSD;
+    }
+
+    function setDexLimits(Dex memory dex_) internal {
+        // Smart Collateral
+        if (dex_.smartCollateral) {
+            SupplyProtocolConfig memory protocolConfigTokenA_ = SupplyProtocolConfig({
+                protocol: dex_.dex,
+                supplyToken: dex_.tokenA,
+                expandPercent: 50 * 1e2, // 50%
+                expandDuration: 1 hours, // 1 hour
+                baseWithdrawalLimitInUSD: dex_.baseWithdrawalLimitInUSD
+            });
+
+            setSupplyProtocolLimits(protocolConfigTokenA_);
+
+            SupplyProtocolConfig memory protocolConfigTokenB_ = SupplyProtocolConfig({
+                protocol: dex_.dex,
+                supplyToken: dex_.tokenB,
+                expandPercent: 50 * 1e2, // 50%
+                expandDuration: 1 hours, // 1 hour
+                baseWithdrawalLimitInUSD: dex_.baseWithdrawalLimitInUSD
+            });
+
+            setSupplyProtocolLimits(protocolConfigTokenB_);
+        }
+
+        // Smart Debt
+        if (dex_.smartDebt) {
+            BorrowProtocolConfig memory protocolConfigTokenA_ = BorrowProtocolConfig({
+                protocol: dex_.dex,
+                borrowToken: dex_.tokenA,
+                expandPercent: 50 * 1e2, // 50%
+                expandDuration: 1 hours, // 1 hour
+                baseBorrowLimitInUSD: dex_.baseBorrowLimitInUSD,
+                maxBorrowLimitInUSD: dex_.maxBorrowLimitInUSD
+            });
+
+            setBorrowProtocolLimits(protocolConfigTokenA_);
+
+            BorrowProtocolConfig memory protocolConfigTokenB_ = BorrowProtocolConfig({
+                protocol: dex_.dex,
+                borrowToken: dex_.tokenB,
+                expandPercent: 50 * 1e2, // 50%
+                expandDuration: 1 hours, // 1 hour
+                baseBorrowLimitInUSD: dex_.baseBorrowLimitInUSD,
+                maxBorrowLimitInUSD: dex_.maxBorrowLimitInUSD
+            });
+
+            setBorrowProtocolLimits(protocolConfigTokenB_);
+        }
+    }
+
+    function setVaultLimits(Vault memory vault_) internal {
+        if (vault_.vaultType == TYPE.TYPE_1) {
+            SupplyProtocolConfig memory protocolConfig_ = SupplyProtocolConfig({
+                protocol: vault_.vault,
+                supplyToken: vault_.supplyToken,
+                expandPercent: 50 * 1e2, // 50%
+                expandDuration: 6 hours, // 6 hours
+                baseWithdrawalLimitInUSD: vault_.baseWithdrawalLimitInUSD
+            });
+
+            setSupplyProtocolLimits(protocolConfig_);
+        }
+
+        if (vault_.vaultType == TYPE.TYPE_1) {
+            BorrowProtocolConfig memory protocolConfig_ = BorrowProtocolConfig({
+                protocol: vault_.vault,
+                borrowToken: vault_.borrowToken,
+                expandPercent: 50 * 1e2, // 50%
+                expandDuration: 6 hours, // 6 hours
+                baseBorrowLimitInUSD: vault_.baseBorrowLimitInUSD,
+                maxBorrowLimitInUSD: vault_.maxBorrowLimitInUSD
+            });
+
+            setBorrowProtocolLimits(protocolConfig_);
+        }
+
+        if (vault_.vaultType == TYPE.TYPE_2) {
+            BorrowProtocolConfig memory protocolConfig_ = BorrowProtocolConfig({
+                protocol: vault_.vault,
+                borrowToken: vault_.borrowToken,
+                expandPercent: 30 * 1e2, // 30%
+                expandDuration: 6 hours, // 6 hours
+                baseBorrowLimitInUSD: vault_.baseBorrowLimitInUSD,
+                maxBorrowLimitInUSD: vault_.maxBorrowLimitInUSD
+            });
+
+            setBorrowProtocolLimits(protocolConfig_);
+        }
+
+        if (vault_.vaultType == TYPE.TYPE_3) {
+            SupplyProtocolConfig memory protocolConfig_ = SupplyProtocolConfig({
+                protocol: vault_.vault,
+                supplyToken: vault_.supplyToken,
+                expandPercent: 35 * 1e2, // 35%
+                expandDuration: 6 hours, // 6 hours
+                baseWithdrawalLimitInUSD: vault_.baseWithdrawalLimitInUSD
+            });
+
+            setSupplyProtocolLimits(protocolConfig_);
+        }
+    }
+
+    // Token Prices Constants
+    uint256 public constant ETH_USD_PRICE = 3_320 * 1e2;
+    uint256 public constant wstETH_USD_PRICE = 3_950 * 1e2;
+    uint256 public constant weETH_USD_PRICE = 3_350 * 1e2;
+    uint256 public constant rsETH_USD_PRICE = 3_750 * 1e2;
+    uint256 public constant weETHs_USD_PRICE = 3_750 * 1e2;
+    uint256 public constant mETH_USD_PRICE = 3_850 * 1e2;
+    uint256 public constant ezETH_USD_PRICE = 3_450 * 1e2;
+
+    uint256 public constant BTC_USD_PRICE = 102_000 * 1e2;
+
+    uint256 public constant STABLE_USD_PRICE = 1 * 1e2;
+    uint256 public constant sUSDe_USD_PRICE = 1.15 * 1e2;
+    uint256 public constant sUSDs_USD_PRICE = 1.02 * 1e2;
+
+    uint256 public constant FLUID_USD_PRICE = 7.2 * 1e2;
+
+    function getRawAmount(
+        address token,
+        uint256 amount,
+        uint256 amountInUSD,
+        bool isSupply
+    ) public view override returns (uint256) {
+        if (amount > 0 && amountInUSD > 0) {
+            revert("both usd and amount are not zero");
+        }
+        uint256 exchangePriceAndConfig_ = LIQUIDITY.readFromStorage(
+            LiquiditySlotsLink.calculateMappingStorageSlot(
+                LiquiditySlotsLink.LIQUIDITY_EXCHANGE_PRICES_MAPPING_SLOT,
+                token
+            )
+        );
+
+        (
+            uint256 supplyExchangePrice,
+            uint256 borrowExchangePrice
+        ) = LiquidityCalcs.calcExchangePrices(exchangePriceAndConfig_);
+
+        uint256 usdPrice = 0;
+        uint256 decimals = 18;
+        if (token == ETH_ADDRESS) {
+            usdPrice = ETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == wstETH_ADDRESS) {
+            usdPrice = wstETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == weETH_ADDRESS) {
+            usdPrice = weETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == rsETH_ADDRESS) {
+            usdPrice = rsETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == weETHs_ADDRESS) {
+            usdPrice = weETHs_USD_PRICE;
+            decimals = 18;
+        } else if (token == mETH_ADDRESS) {
+            usdPrice = mETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == ezETH_ADDRESS) {
+            usdPrice = ezETH_USD_PRICE;
+            decimals = 18;
+        } else if (
+            token == cbBTC_ADDRESS ||
+            token == WBTC_ADDRESS ||
+            token == eBTC_ADDRESS ||
+            token == lBTC_ADDRESS
+        ) {
+            usdPrice = BTC_USD_PRICE;
+            decimals = 8;
+        } else if (token == tBTC_ADDRESS) {
+            usdPrice = BTC_USD_PRICE;
+            decimals = 18;
+        } else if (token == USDC_ADDRESS || token == USDT_ADDRESS) {
+            usdPrice = STABLE_USD_PRICE;
+            decimals = 6;
+        } else if (token == sUSDe_ADDRESS) {
+            usdPrice = sUSDe_USD_PRICE;
+            decimals = 18;
+        } else if (token == sUSDs_ADDRESS) {
+            usdPrice = sUSDs_USD_PRICE;
+            decimals = 18;
+        } else if (
+            token == GHO_ADDRESS ||
+            token == USDe_ADDRESS ||
+            token == deUSD_ADDRESS ||
+            token == USR_ADDRESS ||
+            token == USD0_ADDRESS ||
+            token == fxUSD_ADDRESS ||
+            token == BOLD_ADDRESS
+        ) {
+            usdPrice = STABLE_USD_PRICE;
+            decimals = 18;
+        } else if (token == INST_ADDRESS) {
+            usdPrice = FLUID_USD_PRICE;
+            decimals = 18;
+        } else {
+            revert("not-found");
+        }
+
+        uint256 exchangePrice = isSupply
+            ? supplyExchangePrice
+            : borrowExchangePrice;
+
+        if (amount > 0) {
+            return (amount * 1e12) / exchangePrice;
+        } else {
+            return
+                (amountInUSD * 1e12 * (10 ** decimals)) /
+                ((usdPrice * exchangePrice) / 1e2);
+        }
+    }
+}
