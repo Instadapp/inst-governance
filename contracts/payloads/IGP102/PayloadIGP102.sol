bf697986436110db096a8a25bc3b53d1f793ef28
gite <55936621+xma8lu@users.noreply.github.com>
2025-06-26 20:31:59 +0530

    IGP102

---
+++ contracts/payloads/IGP102/PayloadIGP102.sol
@@ -0,0 +1,341 @@
+pragma solidity ^0.8.21;
+pragma experimental ABIEncoderV2;
+
+import {BigMathMinified} from "../libraries/bigMathMinified.sol";
+import {LiquidityCalcs} from "../libraries/liquidityCalcs.sol";
+import {LiquiditySlotsLink} from "../libraries/liquiditySlotsLink.sol";
+
+import {IGovernorBravo} from "../common/interfaces/IGovernorBravo.sol";
+import {ITimelock} from "../common/interfaces/ITimelock.sol";
+
+import {IFluidLiquidityAdmin, AdminModuleStructs as FluidLiquidityAdminStructs} from "../common/interfaces/IFluidLiquidity.sol";
+import {IFluidReserveContract} from "../common/interfaces/IFluidReserveContract.sol";
+
+import {IFluidVaultFactory} from "../common/interfaces/IFluidVaultFactory.sol";
+import {IFluidDexFactory} from "../common/interfaces/IFluidDexFactory.sol";
+
+import {IFluidDex, IFluidAdminDex, IFluidDexResolver} from "../common/interfaces/IFluidDex.sol";
+
+import {IFluidVault, IFluidVaultT1} from "../common/interfaces/IFluidVault.sol";
+
+import {IFTokenAdmin, ILendingRewards} from "../common/interfaces/IFToken.sol";
+
+import {ISmartLendingAdmin} from "../common/interfaces/ISmartLending.sol";
+import {ISmartLendingFactory} from "../common/interfaces/ISmartLendingFactory.sol";
+import {IFluidLendingFactory} from "../common/interfaces/IFluidLendingFactory.sol";
+
+import {ICodeReader} from "../common/interfaces/ICodeReader.sol";
+
+import {IDSAV2} from "../common/interfaces/IDSA.sol";
+import {IERC20} from "../common/interfaces/IERC20.sol";
+import {IProxy} from "../common/interfaces/IProxy.sol";
+import {PayloadIGPConstants} from "../common/constants.sol";
+import {PayloadIGPHelpers} from "../common/helpers.sol";
+import {PayloadIGPMain} from "../common/main.sol";
+
+contract PayloadIGP102 is PayloadIGPMain {
+    uint256 public constant PROPOSAL_ID = 102;
+
+    function execute() public virtual override {
+        super.execute();
+
+        // Action 1: Set Launch Limits for USDe-USDTb / GHO T2 Vault
+        action1();
+
+        // Action 2: Set Launch Limits for GHO-USDe / GHO T2 Vault
+        action2();
+
+        // Action 3: Remove MS as auth for csUSDL-USDC dex and set rebalancer for csUSDL-USDC smart lending
+        action3();
+
+        // Action 4:
+        action4();
+
+        // Action 5:
+        action5();
+    }
+
+    function verifyProposal() public view override {}
+
+    function _PROPOSAL_ID() internal view override returns (uint256) {
+        return PROPOSAL_ID;
+    }
+
+    /**
+     * |
+     * |     Proposal Payload Actions      |
+     * |__________________________________
+     */
+
+    // @notice Action 1: Set Launch Limits for USDe-USDTb / GHO T2 Vault
+    function action1() internal isActionSkippable(1) {
+        {
+            // launch limits
+            address USDE_USDTb__GHO_VAULT = getVaultAddress(140);
+
+            // USDE-USDTb / GHO T2 vault
+            VaultConfig memory VAULT_USDE_USDTb_GHO = VaultConfig({
+                vault: USDE_USDTb__GHO_VAULT,
+                vaultType: VAULT_TYPE.TYPE_2,
+                supplyToken: address(0), // supply token
+                borrowToken: GHO_ADDRESS,
+                baseWithdrawalLimitInUSD: 0,
+                baseBorrowLimitInUSD: 5_000_000, // $5M
+                maxBorrowLimitInUSD: 10_000_000 // $10M
+            });
+
+            setVaultLimits(VAULT_USDE_USDTb_GHO);
+
+            VAULT_FACTORY.setVaultAuth(
+                USDE_USDTb__GHO_VAULT,
+                TEAM_MULTISIG,
+                false
+            );
+        }
+        {
+            // remove auth
+            address USDE_USDTb_DEX = getDexAddress(36);
+            DEX_FACTORY.setDexAuth(USDE_USDTb_DEX, TEAM_MULTISIG, false);
+        }
+    }
+
+    // @notice Action 2: Set Launch Limits for GHO-USDe / GHO T2 Vault
+    function action2() internal isActionSkippable(2) {
+        {
+            // launch limits
+            address GHO_USDe__GHO_VAULT = getVaultAddress(141);
+
+            // USDE-USDTb / GHO T2 vault
+            VaultConfig memory VAULT_GHO_USDe_GHO = VaultConfig({
+                vault: GHO_USDe__GHO_VAULT,
+                vaultType: VAULT_TYPE.TYPE_2,
+                supplyToken: address(0), // supply token
+                borrowToken: GHO_ADDRESS,
+                baseWithdrawalLimitInUSD: 0,
+                baseBorrowLimitInUSD: 5_000_000, // $5M
+                maxBorrowLimitInUSD: 20_000_000 // $20M
+            });
+
+            setVaultLimits(VAULT_GHO_USDe_GHO);
+
+            VAULT_FACTORY.setVaultAuth(
+                GHO_USDe__GHO_VAULT,
+                TEAM_MULTISIG,
+                false
+            );
+        }
+        {
+            //remove auth
+            address GHO_USDe_DEX = getDexAddress(37);
+            DEX_FACTORY.setDexAuth(GHO_USDe_DEX, TEAM_MULTISIG, false);
+        }
+    }
+
+    // @notice Action 3: Remove MS as auth for csUSDL-USDC dex and set rebalancer for csUSDL-USDC smart lending
+    function action3() internal isActionSkippable(3) {
+        address csUSDL_USDC_DEX = getDexAddress(38);
+        {
+            // csUSDL-USDC DEX
+            DEX_FACTORY.setDexAuth(csUSDL_USDC_DEX, TEAM_MULTISIG, false);
+        }
+        {
+            address fSL38_csUSDL_USDC = getSmartLendingAddress(38);
+
+            // set rebalancer at fSL38 to reserve contract proxy
+            ISmartLendingAdmin(fSL38_csUSDL_USDC).setRebalancer(
+                address(FLUID_RESERVE)
+            );
+        }
+    }
+
+    // @notice Action 4: Collect stETH, wstETH revenue and deposit from reserve contract to Lite Vault
+    function action4() internal isActionSkippable(4) {
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = stETH_ADDRESS;
+        tokens[1] = wstETH_ADDRESS;
+
+        LIQUIDITY.collectRevenue(tokens);
+
+        // Deposit stETH, wstETH into Lite Vault
+        string[] memory targets = new string[](2);
+        bytes[] memory encodedSpells = new bytes[](2);
+
+        string
+            memory depositSignature = "deposit(address,uint256,uint256,uint256)";
+
+        // Spell 1: Deposit stETH into Lite
+        {
+            uint256 STETH_AMOUNT = IERC20(stETH_ADDRESS).balanceOf(
+                address(FLUID_RESERVE)
+            );
+            targets[0] = "BASIC-D-V2";
+            encodedSpells[0] = abi.encodeWithSignature(
+                depositSignature,
+                IETHV2,
+                STETH_AMOUNT,
+                0,
+                0
+            );
+        }
+
+        // Spell 2: Deposit wstETH into Lite
+        {
+            uint256 WSTETH_AMOUNT = IERC20(wstETH_ADDRESS).balanceOf(
+                address(FLUID_RESERVE)
+            );
+            targets[1] = "BASIC-D-V2";
+            encodedSpells[1] = abi.encodeWithSignature(
+                depositSignature,
+                IETHV2,
+                WSTETH_AMOUNT,
+                0,
+                0
+            );
+        }
+
+        IDSAV2(FLUID_RESERVE).cast(targets, encodedSpells, address(this));
+    }
+
+    /**
+     * |
+     * |     Payload Actions End Here      |
+     * |__________________________________
+     */
+
+    // Token Prices Constants
+    uint256 public constant ETH_USD_PRICE = 2_500 * 1e2;
+    uint256 public constant wstETH_USD_PRICE = 3_050 * 1e2;
+    uint256 public constant weETH_USD_PRICE = 2_700 * 1e2;
+    uint256 public constant rsETH_USD_PRICE = 2_650 * 1e2;
+    uint256 public constant weETHs_USD_PRICE = 2_600 * 1e2;
+    uint256 public constant mETH_USD_PRICE = 2_690 * 1e2;
+    uint256 public constant ezETH_USD_PRICE = 2_650 * 1e2;
+
+    uint256 public constant BTC_USD_PRICE = 103_000 * 1e2;
+
+    uint256 public constant STABLE_USD_PRICE = 1 * 1e2;
+    uint256 public constant sUSDe_USD_PRICE = 1.17 * 1e2;
+    uint256 public constant sUSDs_USD_PRICE = 1.05 * 1e2;
+
+    uint256 public constant FLUID_USD_PRICE = 4.2 * 1e2;
+
+    uint256 public constant RLP_USD_PRICE = 1.18 * 1e2;
+    uint256 public constant wstUSR_USD_PRICE = 1.07 * 1e2;
+    uint256 public constant XAUT_USD_PRICE = 3_240 * 1e2;
+    uint256 public constant PAXG_USD_PRICE = 3_240 * 1e2;
+
+    uint256 public constant csUSDL_USD_PRICE = 1.03 * 1e2;
+
+    function getRawAmount(
+        address token,
+        uint256 amount,
+        uint256 amountInUSD,
+        bool isSupply
+    ) public view override returns (uint256) {
+        if (amount > 0 && amountInUSD > 0) {
+            revert("both usd and amount are not zero");
+        }
+        uint256 exchangePriceAndConfig_ = LIQUIDITY.readFromStorage(
+            LiquiditySlotsLink.calculateMappingStorageSlot(
+                LiquiditySlotsLink.LIQUIDITY_EXCHANGE_PRICES_MAPPING_SLOT,
+                token
+            )
+        );
+
+        (
+            uint256 supplyExchangePrice,
+            uint256 borrowExchangePrice
+        ) = LiquidityCalcs.calcExchangePrices(exchangePriceAndConfig_);
+
+        uint256 usdPrice = 0;
+        uint256 decimals = 18;
+        if (token == ETH_ADDRESS) {
+            usdPrice = ETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == wstETH_ADDRESS) {
+            usdPrice = wstETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == weETH_ADDRESS) {
+            usdPrice = weETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == rsETH_ADDRESS) {
+            usdPrice = rsETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == weETHs_ADDRESS) {
+            usdPrice = weETHs_USD_PRICE;
+            decimals = 18;
+        } else if (token == mETH_ADDRESS) {
+            usdPrice = mETH_USD_PRICE;
+            decimals = 18;
+        } else if (token == ezETH_ADDRESS) {
+            usdPrice = ezETH_USD_PRICE;
+            decimals = 18;
+        } else if (
+            token == cbBTC_ADDRESS ||
+            token == WBTC_ADDRESS ||
+            token == eBTC_ADDRESS ||
+            token == lBTC_ADDRESS
+        ) {
+            usdPrice = BTC_USD_PRICE;
+            decimals = 8;
+        } else if (token == tBTC_ADDRESS) {
+            usdPrice = BTC_USD_PRICE;
+            decimals = 18;
+        } else if (token == USDC_ADDRESS || token == USDT_ADDRESS) {
+            usdPrice = STABLE_USD_PRICE;
+            decimals = 6;
+        } else if (token == sUSDe_ADDRESS) {
+            usdPrice = sUSDe_USD_PRICE;
+            decimals = 18;
+        } else if (token == sUSDs_ADDRESS) {
+            usdPrice = sUSDs_USD_PRICE;
+            decimals = 18;
+        } else if (token == csUSDL_ADDRESS) {
+            usdPrice = csUSDL_USD_PRICE;
+            decimals = 18;
+        } else if (
+            token == GHO_ADDRESS ||
+            token == USDe_ADDRESS ||
+            token == deUSD_ADDRESS ||
+            token == USR_ADDRESS ||
+            token == USD0_ADDRESS ||
+            token == fxUSD_ADDRESS ||
+            token == BOLD_ADDRESS ||
+            token == iUSD_ADDRESS ||
+            token == USDTb_ADDRESS
+        ) {
+            usdPrice = STABLE_USD_PRICE;
+            decimals = 18;
+        } else if (token == INST_ADDRESS) {
+            usdPrice = FLUID_USD_PRICE;
+            decimals = 18;
+        } else if (token == wstUSR_ADDRESS) {
+            usdPrice = wstUSR_USD_PRICE;
+            decimals = 18;
+        } else if (token == RLP_ADDRESS) {
+            usdPrice = RLP_USD_PRICE;
+            decimals = 18;
+        } else if (token == XAUT_ADDRESS) {
+            usdPrice = XAUT_USD_PRICE;
+            decimals = 6;
+        } else if (token == PAXG_ADDRESS) {
+            usdPrice = PAXG_USD_PRICE;
+            decimals = 18;
+        } else {
+            revert("not-found");
+        }
+
+        uint256 exchangePrice = isSupply
+            ? supplyExchangePrice
+            : borrowExchangePrice;
+
+        if (amount > 0) {
+            return (amount * 1e12) / exchangePrice;
+        } else {
+            return
+                (amountInUSD * 1e12 * (10 ** decimals)) /
+                ((usdPrice * exchangePrice) / 1e2);
+        }
+    }
+}
